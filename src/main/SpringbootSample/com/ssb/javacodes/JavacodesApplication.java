4b77d5f155a3945f45e715d7e8a38877d452dcf7
Sathish <Sathish@LAPTOP-9V3J1QBM>
2020-01-09 14:16:58 +0530

    Initial commit - Springboot

---
+++ src/main/SpringbootSample/com/ssb/javacodes/JavacodesApplication.java
@@ -0,0 +1,14 @@
+package com.ssb.javacodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodesApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Springboot running");
+		SpringApplication.run(JavacodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssb/javacodes/JavacodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssb.javacodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
